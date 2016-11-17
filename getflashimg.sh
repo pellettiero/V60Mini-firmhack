36e7e90350ee20e9bcb666f7ef0c30bbd6fe15eb
Michele Pellegrini <pellettiero@users.noreply.github.com>
2016-11-17 19:45:24 +0100

    Add checksums and decryption

--- getflashimg.sh
+++ getflashimg.sh
@@ -2,10 +2,12 @@
 # Modified rapidisnake script for v60Mini
 
 fwupd=$1
+sha1sum106="1b91d9e3c63e9c4b429651e44e9fce631b6b7e73"
+sha1sum107="3d81f8f8d5821b14527e1f05a1a3d21276d623d1"
 
 if [ ! -e "$fwupd" ]; then
-	echo "I need firmware as first argument. Please download"
-	echo "it from official V60 facebook page and re-run this."
+	echo "I need firmware (cykb112_v107.exe) as first argument. Please download"
+	echo "it from official KBParadise V60 facebook page and re-run this."
 	exit 0
 fi
 
@@ -16,12 +18,15 @@ if ! make -C rapidisnake/rapiditool; then
 fi
 
 echo "Found usable $fwupd."
+echo "Checking SHA1 for $fwupd."
+echo "3d81f8f8d5821b14527e1f05a1a3d21276d623d1 cykb112_v107.exe" | sha1sum -c - || exit 1
 echo "Extracting obfuscated firmware..."
 mkdir "hack"
 dd status=none if="$fwupd" of="hack/$fwupd.bin.enc" bs=$((0x054000)) skip=1
+echo "De-obfuscating firmware..."
+gcc -o dec dec.c
+./dec da 62 82 cd < hack/$fwupd.bin.enc > hack/$fwupd.dec.bin 
 
-# TODO create de-obfuscator for v60Mini
-# echo "De-obfuscating firmware..."
-# ./rapidisnake/rapiditool/rapiditool -decr "hack/$fwupd.bin.enc" "hack/$fwupd.bin"
+# TODO: what to do with the decrypted file?
 
 echo "All done!"
